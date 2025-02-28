74dbd9045b9c6dbd83993e8d47d849e67e600486
TimOsahenru <timosahenru@gmail.com>
2025-02-28 17:59:30 +0000

    Project setup

---
+++ events/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+# class Event(models.Model):
+    # name = 
+    # description = 
+    # start_date = 
+    # end_date = 
+    # location = 
+    # country = 
+    # event_type = 
---
+++ events/tests/test_views.py
@@ -0,0 +1,2 @@
+def test_dummy_runner():
+    assert 1 + 1 == 2
