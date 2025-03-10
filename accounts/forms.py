604a27a8d2b4ee28b030abf36a9ced3c0782caa7
TimOsahenru <timosahenru@gmail.com>
2025-03-10 10:37:19 +0000

    initial commit

---
+++ accounts/forms.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import User
+from django import forms
+
+# Test clean_password2
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegisterationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'email']
+
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd ['password2']:
+            raise forms.ValidationError('Passwords don\'t match.')
+        return cd['password2']
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from accounts.forms import LoginForm, UserRegisterationForm
+from django.urls import reverse
+from django.contrib.auth import login, authenticate
 
 
 def profile(request):
     return HttpResponse()
+
+
+# def user_login(request):
+#     if request.method == 'POST':
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             user = authenticate(request, username=cd['username'], password=cd['password'])
+
+#             if user is not None:
+#                 if user.is_active:
+#                     login(request, user)
+#                     return HttpResponse('Authenticated successfully')
+#                 else:
+#                     return HttpResponse('Diabled account')
+#             else:
+#                 return HttpResponse('Invalid login')
+#     else:
+#         form = LoginForm()
+#     return render(request, 'login.html', {'form': form})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegisterationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return redirect ('profile')
+            # return render(request, 'register_done_html', {'new_user': new_user})
+        
+    else:
+        user_form = UserRegisterationForm()
+
+    return render(request, 'register.html', {'user_form': user_form})
